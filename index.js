2ad3d8d0e37d8e90a763a55944a9c92480e0ab30
TJ <linux@pop-os.localdomain>
2018-05-11 11:23:52 +0530

    mongodb added

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const keys = require('./config/keys');
+const mongoose = require('mongoose');
+require('./models/User');
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 require('./routes/authRoutes')(app);
