d86fb33675252657fadec6bb00eb96af688ce815
TJ <linux@pop-os.localdomain>
2018-05-11 17:19:59 +0530

    cookies

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,14 +2,20 @@ const passport = require('passport');
 
 module.exports = app => {
 
-app.get('/auth/google', passport.authenticate('google', {
-	scope: ['profile', 'email']
-}));
+	app.get('/auth/google', passport.authenticate('google', {
+		scope: ['profile', 'email']
+	}));
 
-app.get('/auth/google/callback', passport.authenticate('google'));
+	app.get('/auth/google/callback', passport.authenticate('google'));
 
-app.get('/api/current_user', function(req, res){
-	res.send(req.user);
-});
+	app.get('/api/logout', function(req, res){
+		req.logout();
+		//req.user doesn't shows because the user id is killed after logged out
+		res.send(req.user);
+	});
+
+	app.get('/api/current_user', function(req, res){
+		res.send(req.user);
+	});
 
 };
