c3289c6fca2f29f907f2243f8ec1c6398dd94991
TJ <linux@pop-os.localdomain>
2018-05-13 20:07:29 +0530

    materializecss

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
 import Header from './Header';
 
 const Dashboard = () => <h2>Dashboard</h2>;
 const Survey 	= () => <h2>Survey</h2>;
 const Landing 	= () => <h2>Landing</h2>;
 
-const App = () => {
-	return (
-		<div>
-			<BrowserRouter>
-				<div>
-					<Header />
-					<Route exact={true} path="/" component={Landing} />
-					<Route exact={true} path="/surveys" component={Dashboard} />
-					<Route path="/surveys/new" component={Survey} />
-				</div>
-			</BrowserRouter>
-		</div>
-	);
+class App extends Component {
+
+	componentDidMount(){
+		this.props.fetchUser();
+	}
+
+	render(){
+		return (
+			<div className="container">
+				<BrowserRouter>
+					<div>
+						<Header />
+						<Route exact={true} path="/" component={Landing} />
+						<Route exact={true} path="/surveys" component={Dashboard} />
+						<Route path="/surveys/new" component={Survey} />
+					</div>
+				</BrowserRouter>
+			</div>
+		);
+	}
 };
 
-export default App;
+export default connect(null, actions)(App);
